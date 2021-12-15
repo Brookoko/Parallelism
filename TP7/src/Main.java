4f0501e5dd48a0cf69cc21cae1c8eb52cbb3b55f
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-12-16 01:21:10 +0200

    Setup project for lab 8

--- TP7/src/Main.java
+++ TP7/src/Main.java
@@ -1,35 +1,25 @@
 import Matrix.Collective.CollectiveMatrixMultiplication;
-import Matrix.Matrix;
 import Matrix.Simple.SimpleMatrixMultiplication;
-import Matrix.Utils;
 import mpi.MPI;
 
 public class Main
 {
-    private final static int SIZE = 3;
-
     public static void main(String[] args) throws Exception
     {
         MPI.Init(args);
         var simple = new SimpleMatrixMultiplication();
         var collective = new CollectiveMatrixMultiplication();
-        PerformanceChecker performanceChecker = new PerformanceChecker(simple);
+        PerformanceChecker performanceChecker = new PerformanceChecker(collective);
         var rank = MPI.COMM_WORLD.getRank();
-        var a = new Matrix(0, 0);
-        var b = new Matrix(0, 0);
+        var time = performanceChecker.countTimeFor(1500);
+//        var times = performanceChecker.countTimeRepeated(500, 500, 1000);
         if (rank == 0)
         {
-            a = Utils.createSequenceMatrix(SIZE, SIZE);
-            b = Utils.createSequenceMatrix(SIZE, SIZE);
-        }
-        var c1 = new Matrix(SIZE, SIZE);
-        var c2 = new Matrix(SIZE, SIZE);
-        simple.multiply(a, b, c1);
-        collective.multiply(a, b, c2);
-        if (rank == 0)
-        {
-            Utils.print(c1);
-            Utils.print(c2);
+            System.out.println(time);
+//            for (var t : times)
+//            {
+//                System.out.println(t);
+//            }
         }
         MPI.Finalize();
     }
